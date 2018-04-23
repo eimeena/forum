75f367c4c45341a6011c2a7808bbc905429d4345
eimeena <tataeimeena@gmail.com>
2018-04-23 14:59:59 +0200

    version1

---
+++ project/index.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html>
+<title>www.forumdelafrancophonie.org</title>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-amber.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+
+<div class="w3-card-4">
+<style>
+.w3-main{
+
+}
+
+
+
+html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
+.w3-sidebar {
+  z-index: 3;
+  width:150px;
+  top: 61px;
+  bottom: 0;
+  height: 50px;
+}
+body{
+  background-color: none;
+}
+.w3-third{
+  background-color: none;
+  border-color: ;
+  margin-bottom: : 100px;
+  margin-right: 0px;
+}
+.droite{
+  margin-left: 75%;
+}
+.fo{
+  font-size: 10px;
+  width: 50px
+}
+
+.fa{
+
+    font-size: 50px;
+  width: 150px;
+  text-align:center;
+  text-decoration: none;
+  margin: 2px 2px;
+}
+
+.fa:hover {
+    opacity: 0.7;
+}
+
+.fa-facebook{
+  background: #3B5998;
+  color: white;
+}
+
+.fa-twitter {
+  background: #55ACEE;
+  color: white;
+}
+
+.fa-google {
+  background: #dd4b39;
+  color: white;
+}
+
+
+
+.fa-youtube {
+  background: #bb0000;
+  color: white;
+}
+
+.fa-instagram {
+  background: #125688;
+  color: white;
+}
+
+.w3-top{
+height: 50px;
+margin-right: 10px;
+}
+
+.w3-button{
+  margin-bottom:2px;
+  padding-top: 50px;
+  color: white;
+}
+.w3-border{
+  background-color:#87005c;
+    width:350px;
+}
+.w3-twothird{
+  background-color: #b30000;
+  padding-left: 50px;
+}
+
+</style>
+
+</head>
+<body>
+  <!-- Navbar -->
+<div class="w3-top">
+  <div class="w3-bar w3-theme w3-top w3-left-align w3-large">
+
+
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Pourquoi-un-forum?</a>
+
+
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-hover-white">forum2018</a>
+
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Mediatheque</a>
+
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Presse</a>
+
+    <a href="#" class="w3-bar-item w3-button w3-hide-small w3-hide-medium w3-hover-white">partenaires</a>
+
+    <div class="droite">
+<a href="#" class="w3-bar-item w3-button w3-hide-small w3-hide-medium w3-hover-white droite">FAQ</a>
+    </div>
+  </div>
+</div>
+
+<div>
+<div class="w3-dropdown-hover">
+    <button class="w3-button " style="background-color: #666600">Menu</button>
+  <div class="w3-dropdown-content w3-bar-block w3-border">
+    <a href="#" class="w3-bar-item w3-button">Accueil</a>
+    <a href="#" class="w3-bar-item w3-button">Agenda</a>
+    <a href="#" class="w3-bar-item w3-button">contacts</a>
+  </div>
+</div>
+  </div>
+
+
+<!-- Main content: shift it to the right by 250 pixels when the sidebar is visible -->
+<div class="w3-main" style=" margin-left=250px";>
+
+  <div class="w3-row w3-padding-64">
+    <div class="w3-twothird w3-container">
+        <h1 class="w3-text-teal">Heading</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum
+        dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        <img src="forum.jpeg">
+</div>
+
+<div class="w3-third w3-container lol">
+
+    <form class="form-horizontal w3-border w3-padding-64 w3-center " action="/action_page.php">
+      <h2>S'inscrire</h2>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="email">Email:</label>
+      <div class="col-sm-10">
+        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="pwd">Mot de passe:</label>
+      <div class="col-sm-10">
+        <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+      </div>
+    </div>
+
+    <div class="form-group">
+      <div class="col-sm-offset-2 col-sm-10">
+        <div class="checkbox">
+          <label><input type="checkbox" name="remember"> Se rappeler de moi</label>
+        </div>
+      </div>
+    </div>
+    <div class="form-group">
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-default">Souscrire</button>
+      </div>
+    </div>
+  </form>
+    </p>
+  </p>
+
+
+    </div>
+  </div>
+
+  <div class="w3-row">
+    <div class="w3-twothird w3-container">
+      <h1 class="w3-text-teal">Heading</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum
+        dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+<img src="franco.jpeg">
+    </div>
+
+    <div class="w3-third w3-container">
+      <p class="w3-border w3-padding-large w3-padding-30 w3-center">
+
+
+        <iframe width="300" height="300" src="https://www.youtube.com/embed/1X7p7GSrg-U" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+<iframe width="300" height="300" src="https://www.youtube.com/embed/JMtZr8GpSQ8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+<iframe width="300" height="300" src="https://www.youtube.com/embed/Ysi6j2K_a-w" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    </p>
+
+    </div>
+  </div>
+
+  <div class="w3-row w3-padding-64">
+    <div class="w3-twothird w3-container">
+      <h1 class="w3-text-teal">Heading</h1>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum
+        dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  <img src="franco.jpeg">
+</p>
+    </div>
+
+
+  </div>
+
+
+
+  <footer id="myFooter">
+    <div class="w3-container w3-theme-l2 w3-padding-32">
+      <h3>Reseaux Sociaux:</h3>
+
+      <center><p>
+
+<a href="#" class="fa fa-facebook-square"></a>
+<a href="#" class="fa fa-twitter"></a>
+<a href="#" class="fa fa-google"></a>
+<a href="#" class="fa fa-youtube"></a>
+<a href="#" class="fa fa-instagram"></a>
+
+</p></center>
+
+    </div>
+    </div>
+
+    <div class="w3-container w3-theme-l1">
+      <center><p>Powered by <a href="#" target="_blank">EimeenaFall</a></p></center>
+    </div>
+  </footer>
+
+<!-- END MAIN -->
+</div>
+
+<script>
+// Get the Sidebar
+var mySidebar = document.getElementById("mySidebar");
+
+// Get the DIV with overlay effect
+var overlayBg = document.getElementById("myOverlay");
+
+// Toggle between showing and hiding the sidebar, and add overlay effect
+function w3_open() {
+    if (mySidebar.style.display === 'block') {
+        mySidebar.style.display = 'none';
+        overlayBg.style.display = "none";
+    } else {
+        mySidebar.style.display = 'block';
+        overlayBg.style.display = "block";
+    }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+    overlayBg.style.display = "none";
+}
+</script>
+
+</body>
+</html>
